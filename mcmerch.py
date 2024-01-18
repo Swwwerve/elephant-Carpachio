ca8f3c0135cb8cd3550c212d84e942668ff9bf12
Swwwerve <roychr2@mcmaster.ca>
2024-01-18 14:50:25 -0500

    MVP Done

---
+++ mcmerch.py
@@ -0,0 +1,13 @@
+import sys
+
+def main():
+  # Extract the first argument as the price
+  price = float((sys,argv[1]))
+  
+  # Extract the second argument as the quantity
+  quantity = int(sys, argv[2])
+  
+  
+  
+  if __name__ == '__main__':
+    
