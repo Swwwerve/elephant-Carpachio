ceac8a9e2f45419a2f4f9a0a53527b8207640e51
Swwwerve <roychr2@mcmaster.ca>
2024-01-18 14:54:17 -0500

    Actual first MVP (close #1)

--- mcmerch.py
+++ mcmerch.py
@@ -1,13 +1,19 @@
 import sys
 
 def main():
+  
+  tax = {"ON" : 0.13}
   # Extract the first argument as the price
-  price = float((sys,argv[1]))
+  price = float((sys.argv[1]))
   
   # Extract the second argument as the quantity
-  quantity = int(sys, argv[2])
+  quantity = int(sys.argv[2])
   
+  pre_tax = price * quantity
   
+  after_tax = pre_tax * (1 + tax["ON"])
   
-  if __name__ == '__main__':
-    
+  print(after_tax)
+  
+if __name__ == '__main__':
+  main()
